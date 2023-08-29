5874a788f634be0a08756a4f094fd2e019281083
Abdulmojeed <mojeedayoola58@gmail.com>
2023-08-29 03:26:15 +0100

    feat: add an OTP page

---
+++ src/features/Auth/pages/OtpPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface OtpPageProps {
+    
+}
+
+const OtpPage: React.FC<OtpPageProps> = ({  }) => {
+    return (
+        <div>
+            OTP
+        </div>
+    );
+};
+
+export default OtpPage;
